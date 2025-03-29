904b18c7ae35defe355c8e0099884e98cd0e81e8
satya-indra <satyendraa.21@gmail.com>
2025-03-29 11:25:32 +0530

    simple comparator

--- java/examples/src/main/java/org/example/streams/intermediate_operations/Problem_6.java
+++ java/examples/src/main/java/org/example/streams/intermediate_operations/Problem_6.java
@@ -1,14 +1,21 @@
 package org.example.streams.intermediate_operations;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 6. Sort a list of strings in natural order and then in reverse order.
  */
 public class Problem_6 {
     public static void main(String[] args) {
-        List<String> input = new ArrayList<>();
+        List<String> input = List.of("a", "abc", "b", "bcd");
+        List<String> naturalOrder = input.stream().sorted(Comparator
+                .naturalOrder())
+        .toList();
+
+        List<String> reverseOrder = input.stream().sorted(Comparator.reverseOrder()).toList();
 
     }
 }
